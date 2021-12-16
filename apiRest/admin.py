4b896bece2a43623686fadd20f55b685367a46e7
Juan Del Camino <juan.delcamino.r@gmail.com>
2021-12-16 01:58:06 -0600

    se añade app 'apiRest' y los modelos para base de datos

---
+++ apiRest/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from apiRest.models import Activity, Property, Survey
+
+# Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display=('title','created_at_datetime')
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display=('title','created_at_datetime')
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display=('get_activity',)
+
+    def get_activity(self,obj):
+        return obj.activity_id.title
+
+    get_activity.short_description='Encuesta'    
---
+++ apiRest/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.db.models.fields.related import OneToOneField
+
+
+# Create your models here.
+class Property(models.Model):
+    title = models.CharField(max_length=255,null=False, blank=False,verbose_name="título")
+    address = models.TextField(null=False, blank=False,verbose_name="dirección")
+    description = models.TextField(null=False, blank=False,verbose_name="descripción")
+    created_at_datetime= models.DateField(null=False,blank=False, verbose_name="Fecha de creación")
+    updated_at_datetime= models.DateField(null=False,blank=False, verbose_name="Fecha de actualización")
+    disabled_at_datetime= models.DateField(null=True,blank=True, verbose_name="Fecha de deshabilitación")
+    status= models.CharField(max_length=35,null=False,blank=False, verbose_name="estado")
+
+    class Meta:
+        verbose_name="Propiedad"
+        verbose_name_plural="Propiedades"
+        ordering=['-created_at_datetime']
+
+    def __str__(self):
+        return self.title
+    
+class Activity(models.Model):
+    property_id= models.ForeignKey(Property,verbose_name="propiedad",on_delete=models.CASCADE)
+    schedule= models.DateField(null=False,blank=False, verbose_name="Horario")
+    title = models.CharField(max_length=255,null=False, blank=False,verbose_name="título")
+    created_at_datetime= models.DateField(null=False,blank=False, verbose_name="Fecha de creación")
+    updated_at_datetime= models.DateField(null=False,blank=False, verbose_name="Fecha de actualización")
+    status= models.CharField(max_length=35,null=False,blank=False, verbose_name="estado")
+
+    class Meta:
+        verbose_name="Actividad"
+        verbose_name_plural="Actividades"
+        ordering=['-created_at_datetime']
+
+    def __str__(self):
+        return self.title
+    
+
+class Survey(models.Model):
+    activity_id= OneToOneField(Activity,on_delete=models.CASCADE, verbose_name="actividad")
+    answers= JSONField()
+    created_at_datetime= models.DateField(null=False,blank=False, verbose_name="Fecha de creación")
+
+    class Meta:
+        verbose_name="Encuesta"
+        verbose_name_plural="Encuestas"
+        ordering=['-created_at_datetime']
+
+    def __str__(self):
+        return str(self.activity_id.title)
